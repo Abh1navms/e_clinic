8d12c0d0848b1589173333dce7ac0e07be4a08d5
Dhanush-Proj <168286868+Dhanush-Proj@users.noreply.github.com>
2024-12-17 22:23:40 +0530

    another fix

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -7,23 +7,25 @@ const Appointment = () => {
   const { docId } = useParams()
   const { doctors } = useContext(AppContext)
 
-  const [docInfo,setDocInfo] = useState(null)
+  const [docInfo,setDocInfo] = useState([])
 
   const fetchDocInfo = async () => {
-    const docInfo = doctors.find(doc => doc._id === docId)
+    const docInfo = doctors.find(doc => doc._id == docId)
     setDocInfo(docInfo)
    console.log(docInfo)
   }
-
-  useEffect(()=>{
-   fetchDocInfo()
- },[doctors,docId])
-
+   
+   useEffect(()=>{
+     fetchDocInfo();
+    },[doctors,docId])
+    
 
   return (
     <div>
       <div>
         <div>
+          {/* {docInfo.image} */}
+          <label htmlFor="">{docInfo.name}</label>
           <img src={docInfo.image} alt='' />
         </div>
       </div>
